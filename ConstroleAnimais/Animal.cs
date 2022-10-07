7d5784d5e3f740c5305275172dcacdc1a0ebaef1
Daniel Gongo <dan_liveira@outlook.com>
2022-10-07 11:21:06 -0300

    Projeto simples controle animais poo

---
+++ ConstroleAnimais/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstroleAnimais
+{
+    public class Animal
+    {
+        private string _nome;
+        private string _tipo;
+
+        public string Nome
+        {
+            get { return _nome; }
+
+            set
+            {
+                _nome = value.ToUpper();
+            }
+        }
+
+        public string Tipo
+        {
+            get { return _tipo; }
+
+            set
+            {
+                if(value == "cachorro" || value == "gato" || value == "peixe")
+                {
+                    _tipo = value;
+                }
+                else
+                {
+                    Console.WriteLine("Tipo de animal inválido");
+                }
+            }
+        }
+    }
+}
---
+++ ConstroleAnimais/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstroleAnimais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal();
+            Animal a2 = new Animal();
+            Animal a3 = new Animal();
+           
+            int tlCachorro = 0, tlGato = 0, tlPeixe = 0;
+
+            Console.WriteLine("CONTROLE DE ANIMAIS");
+           
+            //Primeiro animal
+            Console.Write("Informe o nome do 1º animal: ");
+            a1.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do 1º animal: ");
+            a1.Tipo = Console.ReadLine();
+
+            if (a1.Tipo.Equals("cachorro")) tlCachorro++;
+            if (a1.Tipo.Equals("gato")) tlGato++;
+            if (a1.Tipo.Equals("peixe")) tlPeixe++;
+
+            //Segundo animal
+            Console.Write("Informe o nome do 2º animal: ");
+            a2.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do 2º animal: ");
+            a2.Tipo = Console.ReadLine();
+
+            if (a2.Tipo.Equals("cachorro")) tlCachorro++;
+            if (a2.Tipo.Equals("gato")) tlGato++;
+            if (a2.Tipo.Equals("peixe")) tlPeixe++;
+
+            //Terceiroo animal
+            Console.Write("Informe o nome do 3º animal: ");
+            a3.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do 3º animal: ");
+            a3.Tipo = Console.ReadLine();
+
+            if (a3.Tipo.Equals("cachorro"))
+            {
+                tlCachorro++;
+            }
+            else if (a3.Tipo.Equals("gato"))
+            {
+                tlGato++;
+            }
+            else
+            {
+                tlPeixe++;
+            }
+
+            Console.WriteLine("Quantidade de animais");
+            Console.WriteLine($"Cachorros:  " + tlCachorro + "\nGatos: " + tlGato + "\nPeixes: " + tlPeixe);
+
+            Console.ReadKey();
+
+        }
+    }
+}
